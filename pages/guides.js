55818fbe819d138fe33fac66b3f0c439f513cd05
Mahum Syed <mahumsye@usc.edu>
2023-09-17 14:22:27 -0700

    try2917

--- pages/guides.js
+++ pages/guides.js
@@ -36,18 +36,31 @@ export default function Guides() {
   },[user, authReady])
 
   return (
-    <div className={styles.guides}>
+    // <div className={styles.guides}>
       
-      {!authReady && <div>Loading...</div>}
+    //   {!authReady && <div>Loading...</div>}
 
-      {error && (
-        <div className={styles.error}>
-          <p>{ error }</p>
-        </div>
-      )}
+    //   {error && (
+    //     <div className={styles.error}>
+    //       <p>{ error }</p>
+    //     </div>
+    //   )}
 
-      {guides && guides.map(guide => (
-        <div key={guide.title} className={styles.card}>
+    //   {guides && guides.map(guide => (
+    //     <div key={guide.title} className={styles.card}>
+            
+
+    //       {/* <h3>{ guide.title }</h3>
+    //       <h4>written by {guide.author}</h4>
+    //       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At corrupti iste ab magnam dignissimos id maxime rerum quae minima. Delectus maxime culpa est consequatur veritatis, perspiciatis cum corrupti possimus quis?</p> */}
+    //     </div>
+    //   ))}
+
+
+
+
+    // </div> 
+
 
 <div
         dangerouslySetInnerHTML={{
@@ -355,16 +368,5 @@ beginner (learn the basics), easy ,intermediate ,advanced
 }}
 />
 
-
-          {/* <h3>{ guide.title }</h3>
-          <h4>written by {guide.author}</h4>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At corrupti iste ab magnam dignissimos id maxime rerum quae minima. Delectus maxime culpa est consequatur veritatis, perspiciatis cum corrupti possimus quis?</p> */}
-        </div>
-      ))}
-
-
-
-
-    </div> 
   )
 }
