39cb3f44fa7e9b17c2bbbab1236f3c95f3bce46a
Mahum Syed <mahumsye@usc.edu>
2023-09-16 18:47:04 -0700

    try3916

--- pages/search.js
+++ pages/search.js
@@ -1,164 +1,57 @@
-
 import { useContext, useEffect, useState } from 'react'
 import styles from '../styles/Guides.module.css'
 import AuthContext from '../stores/authContext'
-import { clicked } from './index.js';
 
-export default function Search() {
+export default function Guides() {
   const { user, authReady, login } = useContext(AuthContext)
   const [guides, setGuides] = useState(null)
   const [error, setError] = useState(null)
 
-  const [clicked, setClicked] = useState(false)
-
-
-
   useEffect(() => {
     if (authReady) {
-      const query = clicked ? '?clicked=true' : ''
-
-      fetch(`/.netlify/functions/search${query}`, {
+      fetch('/.netlify/functions/guides', user && {
         headers: {
-          Authorization: `Bearer ${user.token}` 
+          Authorization:  'Bearer ' + user.token.access_token
         }
       })
       .then(res => {
         if (!res.ok) {
-          return res.json().then(data => {
-            throw new Error(data.message)
-          })
+          login()
+          throw Error('You must be logged in to view this content')
         }
         return res.json()
       })
       .then(data => {
-        setGuides(data)
         setError(null)
+        setGuides(data)
       })
       .catch(err => {
         setError(err.message)
         setGuides(null)
       })
     }
-  }, [user, authReady, clicked])
 
-  const handleClick = () => {
-    setClicked(true)
-  }
- 
+  },[user, authReady])
+
   return (
-
-    <div className="container">
-      <button id="clickme" onClick={handleClick}>
-  Click me  
-</button>
-
+    <div className={styles.guides}>
       
+      {!authReady && <div>Loading...</div>}
 
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-            <!DOCTYPE html>
+      {error && (
+        <div className={styles.error}>
+          <p>{ error }</p>
+        </div>
+      )}
 
-            <html lang="en">
+      {guides && guides.map(guide => (
+        <div key={guide.title} className={styles.card}>
+          <h3>{ guide.title }</h3>
+          <h4>written by {guide.author}</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At corrupti iste ab magnam dignissimos id maxime rerum quae minima. Delectus maxime culpa est consequatur veritatis, perspiciatis cum corrupti possimus quis?</p>
+        </div>
+      ))}
 
-            <head>
-              <meta charset="utf-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <link rel="icon" href="https://glitch.com/favicon.ico" />
-              <title>Crochet Companion</title>
-              <link rel="stylesheet" href="/style.css" />
-              <script src="/script.js" defer></script>
-            </head>
-
-            <body>
-
-              <div id="one">
-                <img id="yuh" src="https://cdn.glitch.global/c6b9db38-a26a-4266-956d-36e2d8c4b185/IMG_0575.jpg?v=1689899431190">
-                <h1>Crochet Companion</h1>
-              </div>
-
-              <ul>
-                <img id="yarn" src='https://thenounproject.com/api/private/icons/643771/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0'>
-
-
-                
-                <img id="yarn2" src='https://thenounproject.com/api/private/icons/643771/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0'>
-              </ul>
-
-              
-            </html>
-          ` 
-        }}
-      />
-
-<nav>
-        <h1>Crochet Companion</h1>
-
-        {authReady && (
-           <ul style={{display: 'flex', justifyContent:'center'}}>
-            <li> <Link href="/">Home</Link> </li> <li> <Link href="/guides">Guides</Link> </li>
-
-            <li>
-      <Link href="/favorites">Favorites</Link>
-    </li>
-            <li>
-              <Link href="/search">Pattern Searchyyy</Link>
-            </li>
-
-            <li>
-              <Link href="/favorites">Favorites</Link> 
-            </li>
-
-            <li>
-              <Link href="/my-uploads">My Uploads</Link>
-            </li>
-            
-            <li>
-              <Link href="/my-creations">My Creations</Link>
-            </li>
-
-            
-            <div className="loginbtn" style={{position: 'absolute', right: 30}}>  {!user && 
-    <li onClick={login}>Login/Signup</li>
-  }
-  </div>
-
-  <div className="email" style={{position: 'absolute', right: 30, top: 170}}>
-
-
-  {user && 
-    <li>{user.email}</li>
-  }
-
-</div>
-<div className="loginbtn" style={{position: 'absolute', right: 30}}>
-  {user &&
-    <li onClick={logout}>Logout</li>
-  }
-  </div>
-
-          </ul>
-        )}
-      </nav>
-
-<div
-        dangerouslySetInnerHTML={{
-          __html: `
-          <p>Welcome to Crochet Companion!</p>
-
-          
-
-          <p>this is the search page.</p>
-
-          <p>which should only be accessible if logged in.</p>
-        </body>
-
-
-
-
-          ` 
-        }}
-      />
-    </div>
-  );
+    </div> 
+  )
 }
